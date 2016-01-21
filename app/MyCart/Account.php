fa0cae2413f5a1569480a507fcc95cb82008ab98
Ken Lin <ken.lin@cloud-interactive.com>
2016-01-21 13:33:49 +0800

    這時才開始一個個補齊相關的 production code 和 DB 欄位等等，讓頁面顯示。

--- app/MyCart/Account.php
+++ app/MyCart/Account.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
-    //
+    protected $fillable = ['name'];
+
 }
--- database/migrations/2016_01_18_131724_create_accounts_table.php
+++ database/migrations/2016_01_18_131724_create_accounts_table.php
@@ -14,6 +14,7 @@ class CreateAccountsTable extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
